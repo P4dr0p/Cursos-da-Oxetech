d9c9e45a4e05a1581e138c1e7ee926e1d226c5cc
Pedro Paulo Ramalho de Almeida <163484462+P4dr0p@users.noreply.github.com>
2026-08-17 11:09:59 -0300

    Add files via upload

---
+++ JavaScript/Aula 02/segundo.js
@@ -0,0 +1,19 @@
+// Variáveis
+let nome;
+let idade;
+let altura;
+
+// Entrada 
+nome = prompt("Informe seu nome: ");
+idade = prompt("Informe seu idade: ");
+altura = prompt("Informe sua altura: ");
+
+// Saída (No navegador)
+alert("Nome: " + nome);
+alert("Idade: " + idade);
+alert("Altura: " + altura);
+
+alert("Nome: " + nome + "\n" + "Idade: " + idade + "\n" + "Altura: " + altura);
+
+// Entrada (navegador)
+confirm("Aceita os termos de uso?");
