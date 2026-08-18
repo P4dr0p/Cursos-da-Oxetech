923b0aa8d520b6d45f972773119391f5696b982d
Pedro Paulo Ramalho de Almeida <163484462+P4dr0p@users.noreply.github.com>
2026-08-18 10:26:53 -0300

    Add files via upload

---
+++ Aula 02/1.js
@@ -0,0 +1,14 @@
+// Variáveis
+let nome;
+let idade;
+let altura;
+
+// Entrada 
+nome = prompt("Informe seu nome: ");
+idade = prompt("Informe seu idade: ");
+altura = prompt("Informe sua altura: ");
+
+// Saída
+console.log("nome: " + nome);
+console.log("idade: " + idade);
+console.log("altura: " + altura);
