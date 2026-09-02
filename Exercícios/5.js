abac4959185812fbb3fe5d88589864d683d11408
Pedro Paulo Ramalho de Almeida <163484462+P4dr0p@users.noreply.github.com>
2026-09-02 10:39:41 -0300

    Delete Exercícios directory

--- Exercícios/5.js
+++
@@ -1,6 +0,0 @@
-// Exercício 5 - Exclusão de Conta
-let deletar = confirm("Deseja realmente deletar sua conta?");
-
-deletar
-    ? alert("Conta deletada com sucesso!")
-    : alert("Ação cancelada pelo usuário.");
